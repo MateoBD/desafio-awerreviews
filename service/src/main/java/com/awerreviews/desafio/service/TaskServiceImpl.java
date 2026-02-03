7a0a8fa2804e48f082f81ab7f753303cb2769ebe
Mateo Buela Daddiego <mateobueladaddiego@gmail.com>
2026-02-03 17:14:28 -0300

    added: Transactional to TaskServiceImpl

--- service/src/main/java/com/awerreviews/desafio/service/TaskServiceImpl.java
+++ service/src/main/java/com/awerreviews/desafio/service/TaskServiceImpl.java
@@ -1,16 +1,16 @@
 package com.awerreviews.desafio.service;
 
-import com.awerreviews.desafio.interfaces.contracts.PageRequest;
 import com.awerreviews.desafio.interfaces.dao.TaskDao;
 import com.awerreviews.desafio.interfaces.service.TaskService;
 import com.awerreviews.desafio.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class TaskServiceImpl implements TaskService {
 
     private final TaskDao taskDao;
@@ -21,11 +21,13 @@ public class TaskServiceImpl implements TaskService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Page<Task> getTasks(PageRequest pageRequest) {
         return taskDao.getTasks(pageRequest);
     }
 
     @Override
+    @Transactional
     public Task createTask(String description) {
         return taskDao.createTask(description);
     }
